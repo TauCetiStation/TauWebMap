e6964b3e1fd06cc71664e2064e49ff4dfcafcb31
Alexey <F678678@gmail.com>
2020-04-21 21:00:00 +0300

    Fixes and update
    Костыльные фиксы. Починил стены и частично столы. В целом на вид  карта выглядит нормально. Оставил одну последнюю рвизию.

---
+++ render/src/main/util/scripts/equal_more_e5c78eb95/smooth_table.var.groovy
@@ -0,0 +1,4 @@
+package scripts.equal_more_e5c78eb95
+
+if (!src.isType('/obj/structure/table'))
+	return
---
+++ render/src/main/util/scripts/equal_more_e5c78eb95/smooth_wall.var.groovy
@@ -0,0 +1,4 @@
+package scripts.equal_more_e5c78eb95
+
+if (!src.isType('/turf/simulated/wall'))
+	return
